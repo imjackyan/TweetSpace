49bb706ad0a65763e07ea08225b66dc2ae2e28a1
David Zhang <zhangh87@mcmaster.ca>
2016-01-16 17:41:19 -0500

    Adds fileIO and TweetObj classes

---
+++ TweetSpace/FileIO.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class FileIO
+    {
+        private static String tweetSep = "'+J8F";
+        public static void write(List<TweetObj> tweets, String path)
+        {
+            StreamWriter file = new StreamWriter(path);
+            for (int i = 0; i < tweets.Count; i++)
+            {
+                file.Write(tweets[i].ToString() + tweetSep);
+            }
+            file.Close();
+        }
+
+        public static List<TweetObj> read(String path)
+        {
+            List<TweetObj> tweets = new List<TweetObj>();
+            String lines = File.ReadAllText(path);
+            String[] text = lines.Split(new String[] { tweetSep }, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < text.Length; i++)
+            {
+                tweets.Add(new TweetObj(text[i]));
+            }
+            return tweets;
+        }
+    }
+}
---
+++ TweetSpace/TweetObj.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tweetinvi.Core.Interfaces.Models;
+using Tweetinvi.Core.Interfaces.Models.Entities;
+using Tweetinvi.Core.Parameters;
+using Tweetinvi.Logic;
+using Tweetinvi.Logic.Model;
+
+namespace ConsoleApplication1
+{
+    class TweetObj
+    {
+        public string text { get; }
+        public string creatorName { get; }
+        public string screenName { get; }
+        public DateTime time { get; }
+        public ICoordinates coords { get; }
+        public String country { get; }
+        public List<String> hashtags { get; }//might be empty
+        private String infoSep = "-dG3=";
+        
+
+        public TweetObj(string text, string creatorName, string screenName, DateTime time, ICoordinates coords, IPlace place, List<IHashtagEntity> hashtags)
+        {
+            this.text = text;
+            this.creatorName = creatorName;
+            this.screenName = screenName;
+            this.time = time;
+            this.coords = coords;
+            this.country = place.Country;
+            this.hashtags = new List<String>();
+            for (int i= 0; i < hashtags.Count; i++)
+            {
+                this.hashtags.Add(hashtags[i].Text);
+            }
+        }
+
+        public TweetObj(string whole)
+        {
+            String [] info = whole.Split(new string[] { infoSep }, StringSplitOptions.RemoveEmptyEntries);
+            this.text = info[0];
+            this.creatorName = info[1];
+            this.screenName = info[2];
+            this.time = DateTime.Parse(info[3]);
+            this.coords = new Coordinates(Double.Parse(info[4]), Double.Parse(info[5]));
+            this.country = info[6];
+            hashtags = new List<String>();
+            if (info.Length == 8){
+                String [] hash = info[7].Split(' ');
+                for (int i = 0; i < hash.Length; i++)
+                {
+                    hashtags.Add(hash[i]);
+                }
+            }
+
+        }
+
+        public override String ToString()
+        {
+            String info = "";
+            info += (text+infoSep+creatorName+infoSep+screenName+infoSep+time.ToString()+
+                infoSep+coords.Latitude+ infoSep+coords.Longitude+ infoSep+country+infoSep);
+            for (int i = 0; i < hashtags.Count; i++)
+            {
+                info += hashtags[i];
+                info += " ";
+            }
+            return info;
+        }
+    }
+}
