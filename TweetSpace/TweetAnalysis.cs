105f48298a0c03959c5d7107da252c22e7f975bb
George Zhang <zhanggq@mcmaster.ca>
2016-01-17 07:49:47 -0800

    Updated filter to search for any occurances of string in tweet - also changes graph behaviour (fixes it for case of empty keyword list)

--- TweetSpace/TweetAnalysis.cs
+++ TweetSpace/TweetAnalysis.cs
@@ -51,6 +51,32 @@ namespace TweetSpace
             {
                 keywords[i] = keywords[i].ToLower();
             }
+            
+            foreach(TweetObj tweet in tweets)
+            {
+                foreach (String word in keywords)
+                {
+                    if (tweet.text.Contains(word))
+                    {
+                        containsKeywords.Add(tweet);
+                        break;
+                    }
+                }
+            }
+            
+            
+            
+            return containsKeywords;
+
+        }
+        //Unused method that only searches for keywords[i] as if it were an individual word
+        public static List<TweetObj> filterTweetsTokened(List<TweetObj> tweets, String[] keywords)
+        {
+            List<TweetObj> containsKeywords = new List<TweetObj>();
+            for (int i = 0; i < keywords.Length; i++)
+            {
+                keywords[i] = keywords[i].ToLower();
+            }
 
             foreach (TweetObj tweet in tweets)
             {
@@ -67,7 +93,6 @@ namespace TweetSpace
             }
 
             return containsKeywords;
-
         }
     }
 }
